d8f03e2cb611eab9a04fc1871c202c50e42f966f
shelarsaurabh13 <shelarsaurabh13@gmail.com>
2026-01-19 22:06:45 +0530

    logger files.

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 
 import org.testng.annotations.BeforeMethod;
+
+import utils.Log;
  
 public class BaseTest {
 
@@ -16,10 +18,11 @@ public class BaseTest {
 
 	public void setup() {
 
+		Log.info("Starting WebDriver....!");
 		driver = new ChromeDriver();
-
 		driver.manage().window().maximize();
 
+		Log.info("Navigetting ot Web URL....!");
 		driver.get("https://admin-demo.nopcommerce.com/login");
 
 	}
